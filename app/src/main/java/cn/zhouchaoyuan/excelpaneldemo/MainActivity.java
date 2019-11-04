96376ba7b15fb79430c549962301c9f902390d82
spissay <sachin.pissay@oracle.com>
2019-11-04 14:37:31 +0530

    introducing AndroidX

--- app/src/main/java/cn/zhouchaoyuan/excelpaneldemo/MainActivity.java
+++ app/src/main/java/cn/zhouchaoyuan/excelpaneldemo/MainActivity.java
@@ -2,12 +2,12 @@ package cn.zhouchaoyuan.excelpaneldemo;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
 import java.util.List;
